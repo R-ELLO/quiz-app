ecc5ad080c47e764ffdcc85679d1e0f3919b2999
Aeriel Waters <aeriel.waters@gmail.com>
2019-11-07 14:24:56 -0600

    js code commit

--- questions-answers.js
+++ questions-answers.js
@@ -1,86 +1,105 @@
 //Questions and answers section
 
-//1.Which element is the most electronegative on the periodic table?
-// Magnesium
-// Fluorine
-// Argon
-// Barium
-
-//The answer is: Flourine
-/* Fluorine is the most electronegative element because it has 5 electrons in its 2p shell. 
-The optimal electron configuration of the 2p orbital contains 6 electrons, 
-so since fluorine is so close to ideal electron configuration, the electrons are held very tightly to the nucleus.*/
-
-//2. Who created the first double-helix DNA model?
-// James Watson & Sherlock Holmes
-// Will Smith & John Legend
-// James Watson & Francis Crick
-// Nelly & Bow Wow
-
-//The answer is: James Watson & Francis Crick
-/* In the early 1950s, American biologist James Watson and British physicist Francis Crick 
-came up with their famous model of the DNA double helix.*/
-
-//3. What year was the Human Genome Project completed?
-// 1997
-// 1979
-// 2015
-// 2003
-
-//The answer is: 2003
-/* In April 2003, researchers successfully completed the Human Genome Project, 
-under budget and more than two years ahead of schedule.*/
-
-//4. What compound makes water?
-// H2O
-// NaOH
-// HCHHCOO
-// H3O
-
-//The answer is: H2O
-/*Made up of two elements, hydrogen and oxygen and chemical formula is H2O. 
-It means two hydrogen molecules are bonded with one oxygen molecule. Water has three states, 
-when liquid it is called water, when gas it is called vapor and when solid it is called ice.*/
-
-//5. Approximately what percentage an adult human body is water?
-// 65%
-// 72%
-// 60%
-// All of the above are possible
-
-//The answer is: All of the above are possible
-/* Approximately 60% to over 72% of the human adult body is water. Generally, an adult male needs about 3 liters 
-(3.2 quarts) per day while an adult female needs about 2.2 liters (2.3 quarts) per day.*/
-
-//6. What is the deepest place on earth?
-// Woodingdean Well
-// Mariana Trench
-// Kola Borehole
-// Lake Superior 
-
-//The answer is: Kola Borehole
-/* The Kola Superdeep Borehole was just 9 inches in diameter, but at 40,230 feet (12,262 meters) reigns as the deepest hole. 
-It took almost 20 years to reach that 7.5-mile depth—only half the distance or less to the mantle.*/
-
-//7. What is the process used by plants to get nutrients from the sun?
-// Mitosis
-// Photosynthesis
-// autosynthesis
-// Eating Food
-
-//The answer is: Photosynthesis
-/* Organisms that use light for the energy needed to make their own food are called producers. In contrast, 
-consumers are creatures that eat producers to get energy. While plants are the best-known producers, algae, cyanobacteria, 
-and some protists also make sugar via photosynthesis.*/
-
-//8. What is the name of the largest ocean?
-// Pacific 
-// Arctic
-// Indian
-// Atlantic
-
-//The answer is: Pacific
-/* The Pacific ocean is the largest and home to locations like the ring of fire is located in the Pacific Ocean basin. 
-A number of volcanoes form a ring around the basin which is where the name comes from. 
-This area is home to many earthquakes due to volcanic activity and the movement of tectonic plates. The Pacific Ocean contains more than 75,000 volcanoes.*/
+const STORE = {
+    questions: [//1
+        {
+            question: 'Which element is the most electronegative on the periodic table?',
+            options:[
+                "Magnesium",
+                "Fluorine",
+                "Argon",
+                "Barium"
+            ],
+            answer: "Flourine",
+            factoid: "Fluorine is the most electronegative element because it has 5 electrons in its 2p shell\.\ 
+            The optimal electron configuration of the 2p orbital contains 6 electrons, so since fluorine is so close to ideal electron configuration, the electrons are held very tightly to the nucleus."
+        },
+        //2
+        {
+            question: "Who created the first double-helix DNA model?",
+            options: [
+                "James Watson & Sherlock Holmes",
+                "Will Smith & John Legend",
+                "James Watson & Francis Crick",
+                "Nelly & Bow Wow"
+            ],
+            answer: "James Watson & Francis Crick",
+            factoid: "In the early 1950s, American biologist James Watson and British physicist Francis Crick came up with their famous model of the DNA double helix."
+        },
+        //3
+        {
+            question: "What year was the Human Genome Project completed?",
+            options: [
+                "1997",
+                "1979",
+                "2015",
+                "2003"
+            ],
+            answer: "2003",
+            factoid: "In April 2003, researchers successfully completed the Human Genome Project, under budget and more than two years ahead of schedule."
+        },
+        //4
+        {
+            question: "What compound makes water?",
+            options: [
+                "H2O",
+                "NaOH",
+                "HCHHCOO",
+                "H3O"
+            ],
+            answer: "H2O",
+            factoid: "Made up of two elements, hydrogen and oxygen and chemical formula is H2O. It means two hydrogen molecules are bonded with one oxygen molecule. Water has three states, when liquid it is called water, when gas it is called vapor and when solid it is called ice."
+        },
+        //5
+        {
+            question: "Approximately what percentage an adult human body is water?",
+            options: [
+                "65%",
+                "72%",
+                "60%",
+                "All of the above are possible"
+            ],
+            answer: "All of the above are possible",
+            factoid: "Approximately 60% to over 72% of the human adult body is water. Generally, an adult male needs about 3 liters (3.2 quarts) per day while an adult female needs about 2.2 liters (2.3 quarts) per day."
+        },
+        //6
+        {
+            question: "What is the deepest place on earth?",
+            options: [
+                "Woodingdean Well",
+                "Mariana Trench",
+                "Kola Borehole",
+                "Lake Superior"
+            ],
+            answer: "Kola Borehole",
+            factoid: "The Kola Superdeep Borehole was just 9 inches in diameter, but at 40,230 feet (12,262 meters) reigns as the deepest hole. It took almost 20 years to reach that 7.5-mile depth—only half the distance or less to the mantle."
+        },
+        //7
+        {
+            question: "What is the process used by plants to get nutrients from the sun?",
+            options: [
+                "Mitosis",
+                "Photosynthesis",
+                "autosynthesis",
+                "Eting Food"
+            ],
+            answer: "Photosynthesis",
+            factoid: "Organisms that use light for the energy needed to make their own food are called producers. In contrast, consumers are creatures that eat producers to get energy. While plants are the best-known producers, algae, cyanobacteria, and some protists also make sugar via photosynthesis."
+        },
+        //8
+        {
+            question: "What is the name of the largest ocean?",
+            options: [
+                "Pacific",
+                "Arctic",
+                "Indian",
+                "Atlantic"
+            ],
+            answer: "Pacific",
+            factoid: "The Pacific ocean is the largest and home to locations like the ring of fire is located in the Pacific Ocean basin. A number of volcanoes form a ring around the basin which is where the name comes from. This area is home to many earthquakes due to volcanic activity and the movement of tectonic plates. The Pacific Ocean contains more than 75,000 volcanoes."
+        },
+    ],
+    currentQuestion: 0,
+    score: 0
+};
 
