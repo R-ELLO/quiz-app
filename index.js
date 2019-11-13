aa2430d9c36bc27f8e8deffc0b0763271e6ffb60
Aeriel Waters <aeriel.waters@gmail.com>
2019-11-13 08:54:49 -0600

    css focus commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Start quiz button on start page and on end page to start quiz
+
+function startQuiz() {
+    $('#startbtn').on('click', function(event){
+      populateQuestion();
+    }
+    );
+}
+
